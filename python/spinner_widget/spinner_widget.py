665c7b8ce42a1cb35f39872b797987218896f636
Alan Dann <alan.dann@shotgunsoftware.com>
2015-02-20 17:12:07 +0000

    For #28107 - Added spinner widget to the framework - Implements a simple animated spinner that is controlled via the visibility of the widget itself.

---
+++ python/spinner_widget/spinner_widget.py
@@ -0,0 +1,109 @@
+# Copyright (c) 2015 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""
+
+"""
+
+import time
+import math
+
+import sgtk
+from sgtk.platform.qt import QtCore, QtGui
+
+class SpinnerWidget(QtGui.QWidget):
+    """
+    A widget that shows an animated spinner whilst visible
+    """
+    def __init__(self, parent=None):
+        """
+        """
+        QtGui.QWidget.__init__(self, parent)
+        
+        # public properties:
+        self.fps = 20
+        self.border = 2
+        self.line_width = 2
+        self.arc_length = 300
+        self.seconds_per_spin = 3
+        
+        # timer used to force a repaint:
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self._on_timer_timeout)
+        
+        # keep track of the current start angle to avoid 
+        # unnecessary repaints
+        self._start_angle = 0
+        
+    def paintEvent(self, event):
+        """
+        """
+        # call the base paint event:
+        QtGui.QWidget.paintEvent(self, event)
+        
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        try:
+            painter.setRenderHint(QtGui.QPainter.Antialiasing)
+            
+            # TODO - replace with widget foreground color
+            pen = QtGui.QPen(QtGui.QColor(200, 200, 200))
+            pen.setWidth(self.line_width)
+            painter.setPen(pen)
+            
+            border = self.border + int(math.ceil(self.line_width / 2.0))
+            r = QtCore.QRect(0, 0, self.width(), self.height())
+            r.adjust(border, border, -border, -border)
+            
+            # draw the arc:    
+            painter.drawArc(r, -self._start_angle * 16, self.arc_length * 16)
+            
+        finally:
+            painter.end()        
+
+    def showEvent(self, event):
+        """
+        """
+        if not self._timer.isActive():
+            self._timer.start(1000 / max(1, self.fps))
+        QtGui.QWidget.showEvent(self, event)
+    
+    def hideEvent(self, event):
+        """
+        """
+        self._timer.stop()
+        QtGui.QWidget.hideEvent(self, event)
+
+    def closeEvent(self, event):
+        """
+        """
+        self._timer.stop()
+        QtGui.QWidget.closeEvent(self, event)
+    
+    def _on_timer_timeout(self):
+        """
+        """
+        if not self.isVisible():
+            return
+        
+        # calculate the spin angle as a function of the current time so that all spinners appear in sync!
+        t = time.time()
+        whole_seconds = int(t)
+        p = (whole_seconds % self.seconds_per_spin) + (t - whole_seconds)
+        angle = int((360 * p)/self.seconds_per_spin)
+        
+        if angle == self._start_angle:
+            # angle hasn't changed!
+            return
+        
+        self._start_angle = angle
+        
+        # trigger a repaint:
+        self.update()
