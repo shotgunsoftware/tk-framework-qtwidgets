3408191ed548e52c70c9c42fa6d626faf4b4160b
Alan Dann <alan.dann@shotgunsoftware.com>
2015-02-23 11:57:23 +0000

    For #28107 - moved hierarchical filtering proxy model into framework - This proxy model provides efficient filtering across a hierarchical source model

--- python/models/__init__.py
+++ python/models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Shotgun Software Inc.
+# Copyright (c) 2015 Shotgun Software Inc.
 # 
 # CONFIDENTIAL AND PROPRIETARY
 # 
@@ -8,10 +8,6 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-from . import views
-from . import models
-from . import shotgun_view
-from . import overlay_widget
-from . import help_screen
-from . import spinner_widget
-from . import search_widget
+# Proxy model that allows for efficient hierarhcical filtering of a tree-based 
+# source model
+from .hierarchical_filtering_proxy_model import HierarchicalFilteringProxyModel
