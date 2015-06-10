3c2ae593c530176be557595066e700185c3da6b1
Alan Dann <alan.dann@shotgunsoftware.com>
2015-06-10 12:43:27 +0100

    HierarchicalFilterProxyModel now filters by model index instead of item - Previously it resolved indexes to items which meant that the filtering broke when the source model wasn't a QStandardItemModel.  Now only indexes are used so the proxy model can be attached to any source model deriving from QAbstractItemModel (including other proxy models). - Deprecated protected method '_is_item_accepted' and replaced with a new method '_is_row_accepted' which should now be overriden in derived classes.

--- python/models/hierarchical_filtering_proxy_model.py
+++ python/models/hierarchical_filtering_proxy_model.py
@@ -11,8 +11,6 @@
 """
 Proxy model that provides efficient hierarhcical filtering of a tree-based source model
 """
-import weakref
-    
 import sgtk
 from sgtk.platform.qt import QtCore, QtGui
 
@@ -21,38 +19,40 @@ class HierarchicalFilteringProxyModel(QtGui.QSortFilterProxyModel):
     Inherited from a QSortFilterProxyModel, this class implements filtering across all 
     levels of the hierarchy in a hierarchical (tree-based) model
     """
-    
-    # The caches used to optimise filtering use weak refs which will outlive the items
+
+    # The caches used to optimise filtering use QPersistenModelIndexes which will outlive the items
     # they reference and would lead to an ever-growing cache if not cleaned.  This
-    # value determines how often the caches should be cleaned/trimmed of invalid refs.
+    # value determines how often the caches should be cleaned/trimmed of invalid indexes.
     _MAX_FILTERS_BEFORE_CACHE_CLEAN = 256
-    
+
     def __init__(self, parent=None):
         """
         Construction
         :param parent:    The parent QObject to use for this instance
         """
         QtGui.QSortFilterProxyModel.__init__(self, parent)
-        
+
         self._cached_regexp = None
         self._filter_dirty = True
         self._accepted_cache = {}
         self._child_accepted_cache = {}
-        
+
         self._filter_count = 0
 
-    def _is_item_accepted(self, item, parent_accepted):
+    def _is_row_accepted(self, src_row, src_parent_idx, parent_accepted):
         """
-        Override this method to decide if the specified item should be accepted or 
-        not by the filter.
-        
+        Override this method to decide if the specified row should be accepted or not by
+        the filter.
+
         This should be overridden instead of filterAcceptsRow in derived classes
-        
-        :param item:            The QStandardItem item
+
+        :param src_row:         The row in the source model to filter
+        :param src_parent_idx:  The parent QModelIndex instance to filter
         :param parent_accepted: True if a parent item has been accepted by the filter
-        :returns:               True if this item should be accepted, otherwise False
+        :returns:               True if this index should be accepted, otherwise False
         """
-        raise NotImplementedError()
+        raise NotImplementedError("HierarchicalFilteringProxyModel._is_row_accepted() must be overridden"
+                                  " in derived classes!")
 
     # -------------------------------------------------------------------------------
     # -------------------------------------------------------------------------------
@@ -69,7 +69,7 @@ class HierarchicalFilteringProxyModel(QtGui.QSortFilterProxyModel):
         """
         Overriden base class method used to determine if a row is accepted by the
         current filter.
-        
+
         This implementation checks both up and down the hierarchy to determine if
         this row should be accepted.
 
@@ -87,28 +87,20 @@ class HierarchicalFilteringProxyModel(QtGui.QSortFilterProxyModel):
             self._filter_dirty = False
             self._filter_count = 0
         elif self._filter_count > HierarchicalFilteringProxyModel._MAX_FILTERS_BEFORE_CACHE_CLEAN:
-            # clear out any invalid weak refs from the cache
-            # to ensure it doesn't grow insanely big!
-            new_cache = {}
-            for k, v in self._accepted_cache.iteritems():
-                if k() is not None:
-                    new_cache[k] = v
-            self._accepted_cache = new_cache
-            new_cache = {}
-            for k, v in self._child_accepted_cache.iteritems():
-                if k() is not None:
-                    new_cache[k] = v
-            self._child_accepted_cache = new_cache
+            # clear out any invalid indexes from the cache to ensure it doesn't grow insanely big!
+            self._accepted_cache = dict([(k, v) for k, v in self._accepted_cache.iteritems() 
+                                            if k.isValid()])
+            self._child_accepted_cache = dict([(k, v) for k, v in self._child_accepted_cache.iteritems() 
+                                                    if k.isValid()])
             self._filter_count = 0
         self._filter_count += 1
-        
+
         # get the source index for the row:
         src_model = self.sourceModel()
         src_idx = src_model.index(src_row, 0, src_parent_idx)
-        src_item = src_model.itemFromIndex(src_idx)
-        
+
         # first, see if any children of this item are known to already be accepted
-        child_accepted = self._child_accepted_cache.get(weakref.ref(src_item))
+        child_accepted = self._child_accepted_cache.get(src_idx)
         if child_accepted == True:
             # child is accepted so this item must also be accepted
             return True
@@ -116,69 +108,70 @@ class HierarchicalFilteringProxyModel(QtGui.QSortFilterProxyModel):
         # next, we need to determine if the parent item has been accepted.  To do this,
         # search up the hierarchy stopping at the first parent that we know for sure if
         # it has been accepted or not.
-        upstream_items = []
+        upstream_indexes = []
         current_idx = src_idx
         parent_accepted = False
         while current_idx and current_idx.isValid():
-            item = src_model.itemFromIndex(current_idx)
-            accepted = self._accepted_cache.get(weakref.ref(item))
+            accepted = self._accepted_cache.get(current_idx)
             if accepted != None:
                 parent_accepted = accepted
                 break
-            upstream_items.append(item)
+            upstream_indexes.append(current_idx)
             current_idx = current_idx.parent()
-            
+
         # now update the accepted status for items that we don't know
         # for sure:
-        for item in reversed(upstream_items):
-            accepted = self._is_item_accepted(item, parent_accepted)
-            self._accepted_cache[weakref.ref(item)] = accepted
+        for idx in reversed(upstream_indexes):
+            accepted = self._is_row_accepted(idx.row(), idx.parent(), parent_accepted)
+            self._accepted_cache[QtCore.QPersistentModelIndex(idx)] = accepted
             parent_accepted = accepted
 
-        if src_item.hasChildren():
+        if src_model.hasChildren(src_idx):
             # the parent acceptance doesn't mean that it is filtered out as this
             # depends if there are any children accepted:            
-            return self._is_child_accepted_r(src_item, parent_accepted)
+            return self._is_child_accepted_r(src_idx, parent_accepted)
         else:
             return parent_accepted  
 
-    def _is_child_accepted_r(self, item, parent_accepted):
+    def _is_child_accepted_r(self, idx, parent_accepted):
         """
         Recursively check children to see if any of them have been accepted.
 
-        :param item:            The item whose children should be checked
+        :param idx:             The model index whose children should be checked
         :param parent_accepted: True if a parent item has been accepted
         :returns:               True if a child of the item is accepted by the filter
         """
+        model = idx.model()
+
         # check to see if any children of this item are known to have been accepted:
-        child_accepted = self._child_accepted_cache.get(weakref.ref(item))
+        child_accepted = self._child_accepted_cache.get(idx)
         if child_accepted != None:
             # we already computed this so just return the result
             return child_accepted
-        
+
         # need to recursively iterate over children looking for one that is accepted:
         child_accepted = False
-        for ci in range(item.rowCount()):
-            child_item = item.child(ci)
-            
+        for ci in range(model.rowCount(idx)):
+            child_idx = idx.child(ci, 0)
+
             # check if child item is in cache:
-            accepted = self._accepted_cache.get(weakref.ref(child_item))
+            accepted = self._accepted_cache.get(child_idx)
             if accepted == None:
                 # it's not so lets see if it's accepted and add to the cache:
-                accepted = self._is_item_accepted(child_item, parent_accepted)
-                self._accepted_cache[weakref.ref(child_item)] = accepted
+                accepted = self._is_row_accepted(child_idx.row(), idx, parent_accepted)
+                self._accepted_cache[QtCore.QPersistentModelIndex(child_idx)] = accepted
 
-            if child_item.hasChildren():
-                child_accepted = self._is_child_accepted_r(child_item, accepted)
+            if model.hasChildren(child_idx):
+                child_accepted = self._is_child_accepted_r(child_idx, accepted)
             else:
                 child_accepted = accepted
-                
+
             if child_accepted:
                 # found a child that was accepted so we can stop searching
                 break
 
         # cache if any children were accepted:
-        self._child_accepted_cache[weakref.ref(item)] = child_accepted
+        self._child_accepted_cache[QtCore.QPersistentModelIndex(idx)] = child_accepted
         return child_accepted    
 
     def setSourceModel(self, model):
@@ -222,21 +215,17 @@ class HierarchicalFilteringProxyModel(QtGui.QSortFilterProxyModel):
         # clear all rows from the accepted caches
         for row in range(start_idx.row(), end_idx.row()+1):
             idx = self.sourceModel().index(row, 0, parent_idx)
-            item = self.sourceModel().itemFromIndex(idx)
-            item_ref = weakref.ref(item)
-            if item_ref in self._child_accepted_cache:
-                del self._child_accepted_cache[item_ref]
-            if item_ref in self._accepted_cache:
-                del self._accepted_cache[item_ref]
+            if idx in self._child_accepted_cache:
+                del self._child_accepted_cache[idx]
+            if idx in self._accepted_cache:
+                del self._accepted_cache[idx]
 
         # remove parent hierarchy from caches as well:
         while parent_idx.isValid():
-            item = self.sourceModel().itemFromIndex(parent_idx)
-            item_ref = weakref.ref(item)
-            if item_ref in self._child_accepted_cache:
-                del self._child_accepted_cache[item_ref]
-            if item_ref in self._accepted_cache:
-                del self._accepted_cache[item_ref]
+            if parent_idx in self._child_accepted_cache:
+                del self._child_accepted_cache[parent_idx]
+            if parent_idx in self._accepted_cache:
+                del self._accepted_cache[parent_idx]
             parent_idx = parent_idx.parent()
 
     def _on_source_model_rows_inserted(self, parent_idx, start, end):
@@ -263,11 +252,7 @@ class HierarchicalFilteringProxyModel(QtGui.QSortFilterProxyModel):
         if not parent_idx.isValid():
             return
 
-        parent_item = self.sourceModel().itemFromIndex(parent_idx)
-        if not parent_item:
-            return
-
-        accepted = self._accepted_cache.get(weakref.ref(parent_item))
+        accepted = self._accepted_cache.get(parent_idx)
         if accepted == False:
             # the parent item has previously been filtered out but adding new children might
             # change this so lets invalidate everything forcing an update of the proxy model
