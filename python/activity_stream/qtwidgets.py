2424179dc1261c577d358787d4377ebc6a857fa0
Manne Öhrström <manne@shotgunsoftware.com>
2015-10-12 14:17:22 +0100

    Updated version label name.

--- python/activity_stream/qtwidgets.py
+++ python/activity_stream/qtwidgets.py
@@ -18,5 +18,5 @@ import sgtk
 note_input_widget = sgtk.platform.current_bundle().import_module("note_input_widget")
 NoteInputWidget = note_input_widget.NoteInputWidget
 
-version_label = sgtk.platform.current_bundle().import_module("version_label")
-VersionLabel = version_label.VersionLabel
+playback_label = sgtk.platform.import_framework("tk-framework-qtwidgets", "playback_label")
+ShotgunPlaybackLabel = playback_label.ShotgunPlaybackLabel
