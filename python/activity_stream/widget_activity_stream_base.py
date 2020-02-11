47eabb239f4129f837e5ee67682885dbd826ead6
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2020-02-11 15:32:11 -0500

    fixes rounding errors in the activity stream day count

--- python/activity_stream/widget_activity_stream_base.py
+++ python/activity_stream/widget_activity_stream_base.py
@@ -182,26 +182,26 @@ class ActivityStreamBaseWidget(QtGui.QWidget):
             if second_diff < 10:
                 return "just now"
             if second_diff < 60:
-                return str(second_diff) + " seconds ago"
+                return "%d" % (second_diff,) + " seconds ago"
             if second_diff < 120:
                 return "a minute ago"
             if second_diff < 3600:
-                return str(second_diff / 60) + " minutes ago"
+                return str(second_diff // 60) + " minutes ago"
             if second_diff < 7200:
                 return "an hour ago"
             if second_diff < 86400:
-                return str(second_diff / 3600) + " hours ago"
+                return str(second_diff // 3600) + " hours ago"
         if day_diff == 1:
             return "Yesterday"
         if day_diff < 7:
             return str(day_diff) + " days ago"
         if day_diff < 31:
-            return str(day_diff / 7) + " weeks ago"
+            return str(day_diff // 7) + " weeks ago"
         if day_diff < 365:
-            if day_diff / 30 == 1:
+            if day_diff // 30 == 1:
                 return "1 month ago"
-            return str(day_diff / 30) + " months ago"
-        return str(day_diff / 365) + " years ago"
+            return str(day_diff // 30) + " months ago"
+        return str(day_diff // 365) + " years ago"
 
     def __generate_url(self, entity_type, entity_id, name):
         """
