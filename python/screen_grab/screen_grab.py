4f55d5621cb81c8c5258580aca8b7edf72b72092
Joseph Yu <joseph.yu@wwfx.co.uk>
2020-04-23 17:31:15 +0100

    Qt 4/5 specific screen grab routines
    Squash of wwfxuk's fork: 3677f70 and e9b03bc

--- python/screen_grab/screen_grab.py
+++ python/screen_grab/screen_grab.py
@@ -15,6 +15,8 @@ import os
 from sgtk.platform.qt import QtCore, QtGui
 import sgtk
 
+QT_MAJOR = int(QtCore.qVersion().split(".")[0])  # Works for PySide* and PyQt*
+
 
 class ScreenGrabber(QtGui.QDialog):
     """
@@ -332,10 +334,19 @@ def get_desktop_pixmap(rect):
     :returns: Captured image
     :rtype: :class:`~PySide.QtGui.QPixmap`
     """
-    desktop = QtGui.QApplication.desktop()
-    return QtGui.QPixmap.grabWindow(
-        desktop.winId(), rect.x(), rect.y(), rect.width(), rect.height()
-    )
+    x_y_w_h = rect.x(), rect.y(), rect.width(), rect.height()
+
+    if QT_MAJOR == 5:
+        screen = QtGui.QApplication.primaryScreen()
+        pixmap = screen.grabWindow(0, *x_y_w_h)
+    elif QT_MAJOR == 4:
+        desktop = QtGui.QApplication.desktop()
+        pixmap = QtGui.QPixmap.grabWindow(desktop.winId(), *x_y_w_h)
+    else:
+        message = "Screen capture not implmented for Qt %d"
+        raise NotImplementedError(message % QT_MAJOR)
+
+    return pixmap
 
 
 # Backwards compatibility, as this used to be a module-level
