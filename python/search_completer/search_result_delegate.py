76ca9a2bc3350ab14554741b513c97500bfe0646
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2020-11-11 08:49:02 -0500

    ensure string is utf8 string, not unicode object

--- python/search_completer/search_result_delegate.py
+++ python/search_completer/search_result_delegate.py
@@ -9,6 +9,7 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import sgtk
+from tank_vendor import six
 
 # import the shotgun_model and view modules from the shotgun utils framework
 shotgun_model = sgtk.platform.import_framework(
@@ -38,7 +39,7 @@ class SearchResultDelegate(views.WidgetDelegate):
         super(SearchResultDelegate, self).__init__(view)
 
         self._pixmaps = CompleterPixmaps()
-        self._text = text
+        self._text = six.ensure_str(text)
 
         self.selection_model = view.selectionModel()
 
