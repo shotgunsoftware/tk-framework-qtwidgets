b89cb237a0d47e977bc36697442f370750b59803
Rob Aitchison <rob.aitchison@autodesk.com>
2021-01-11 15:47:29 -0500

    Added device_ratio check to screen_grab.py. All other file changes were pre-commit linter.

--- python/screen_grab/screen_grab.py
+++ python/screen_grab/screen_grab.py
@@ -333,8 +333,13 @@ def get_desktop_pixmap(rect):
     :rtype: :class:`~PySide.QtGui.QPixmap`
     """
     desktop = QtGui.QApplication.desktop()
+    device_ratio = QtGui.QApplication.devicePixelRatio(QtGui.QApplication.instance())
     return QtGui.QPixmap.grabWindow(
-        desktop.winId(), rect.x(), rect.y(), rect.width(), rect.height()
+        desktop.winId(),
+        rect.x() * device_ratio,
+        rect.y() * device_ratio,
+        rect.width() * device_ratio,
+        rect.height() * device_ratio,
     )
 
 
