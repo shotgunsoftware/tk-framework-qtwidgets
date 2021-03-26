e6834ba8dc861a39d56d2eb73d6bb006f17f1aa5
Stacey Oue <stacey.oue@autodesk.com>
2021-03-26 13:47:26 -0400

    Add shotgun widget classes.

---
+++ python/shotgun_widget/shotgun_widget.py
@@ -0,0 +1,134 @@
+# Copyright (c) 2020 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""
+A simple widget to display Shotgun information.
+The information we want to display can be formatted using the following rules:
+
+    {[preroll]shotgun.field.name|sg_field_name_fallback::directive[postroll]}
+
+Basic Examples:
+
+- {code}                         # simple format
+- {sg_sequence.Sequence.code}    # deep links
+- {artist|created_by}            # if artist is null, use created_by
+
+Directives are also supported - these are used by the formatting logic
+and include the following:
+
+- {sg_sequence::showtype}        # will generate a link saying
+                                 # 'Sequence ABC123' instead of just
+                                 # 'ABC123' like it does by default
+- {sg_sequence::nolink}          # no url link will be created
+
+Optional pre/post roll - if a value is null, pre- and post-strings are
+omitted from the final result. Examples of this syntax:
+
+- {[Name: ]code}                 # If code is set, 'Name: xxx' will be
+                                 # printed out, otherwise nothing.
+- {[Name: ]code[<br>]}           # Same but with a post line break
+"""
+
+import sgtk
+from sgtk.platform.qt import QtCore, QtGui
+
+
+shotgun_globals = sgtk.platform.import_framework(
+    "tk-framework-shotgunutils", "shotgun_globals",
+)
+
+logger = sgtk.platform.get_logger(__name__)
+
+
+class ShotgunWidget(QtGui.QWidget):
+    """
+    Shotgun widget base class.
+    """
+
+    def __init__(self, parent=None):
+        """
+        Construction
+
+        :param parent: The parent widget
+        """
+        QtGui.QWidget.__init__(self, parent)
+
+        self._thumbnail = True
+        self._sg_fields = []
+
+        # compute hilight colors
+        p = QtGui.QPalette()
+        highlight_col = p.color(QtGui.QPalette.Active, QtGui.QPalette.Highlight)
+        self._highlight_str = "rgb(%s, %s, %s)" % (
+            highlight_col.red(),
+            highlight_col.green(),
+            highlight_col.blue(),
+        )
+        self._transp_highlight_str = "rgba(%s, %s, %s, 25%%)" % (
+            highlight_col.red(),
+            highlight_col.green(),
+            highlight_col.blue(),
+        )
+
+        self._menu = QtGui.QMenu()
+        self._actions = []
+
+    def set_thumbnail(self, thumbnail):
+        """
+        Set the widget thumbnail. If the widget has been configured to display a thumbnail but no image has been
+        supplied, a default picture will be displayed instead.
+
+        :param thumbnail: The thumbnail as a QtGui.QIcon
+        """
+
+        if not self._thumbnail:
+            return
+
+        if not thumbnail:
+            pixmap = QtGui.QPixmap(
+                ":/tk-framework-qtwidgets/shotgun_widget/rect_512x400.png"
+            )
+        else:
+            pixmap = thumbnail.pixmap(512)
+
+        self._ui.thumbnail.setPixmap(pixmap)
+        self._ui.thumbnail.setVisible(True)
+
+    def set_selected(self, selected):
+        """
+        Adjust the style sheet to indicate selection or not
+
+        :param selected: True if selected, false if not
+        """
+        if selected:
+            self._ui.box.setStyleSheet(
+                """#box {border-width: 2px;
+                                                 border-color: %s;
+                                                 border-style: solid;
+                                                 background-color: %s}
+                                      """
+                % (self._highlight_str, self._transp_highlight_str)
+            )
+            if self._actions:
+                self._ui.button.setVisible(True)
+
+        else:
+            self._ui.box.setStyleSheet("")
+            self._ui.button.setVisible(False)
+
+    def set_actions(self, actions):
+        """
+        Adds a list of QActions to the actions menu for this widget.
+
+        :param actions: List of QActions to add
+        """
+        self._actions = actions
+        for a in self._actions:
+            self._menu.addAction(a)
---
+++ python/shotgun_widget/ui/shotgun_folder_widget.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'shotgun_folder_widget.ui'
+#
+#      by: pyside-uic 0.2.15 running on PySide 1.2.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from tank.platform.qt import QtCore, QtGui
+
+class Ui_ShotgunFolderWidget(object):
+    def setupUi(self, ShotgunFolderWidget):
+        ShotgunFolderWidget.setObjectName("ShotgunFolderWidget")
+        ShotgunFolderWidget.resize(160, 161)
+        self.verticalLayout = QtGui.QVBoxLayout(ShotgunFolderWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.box = QtGui.QFrame(ShotgunFolderWidget)
+        self.box.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.box.setFrameShadow(QtGui.QFrame.Raised)
+        self.box.setObjectName("box")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.box)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.thumbnail_layout = QtGui.QHBoxLayout()
+        self.thumbnail_layout.setContentsMargins(0, 0, 0, 0)
+        self.thumbnail_layout.setObjectName("thumbnail_layout")
+        self.thumbnail = QtGui.QLabel(self.box)
+        self.thumbnail.setMinimumSize(QtCore.QSize(96, 75))
+        self.thumbnail.setMaximumSize(QtCore.QSize(96, 75))
+        self.thumbnail.setText("")
+        self.thumbnail.setPixmap(QtGui.QPixmap(":/tk-framework-qtwidgets/shotgun_widget/rect_512x400.png"))
+        self.thumbnail.setScaledContents(True)
+        self.thumbnail.setObjectName("thumbnail")
+        self.thumbnail_layout.addWidget(self.thumbnail)
+        self.verticalLayout_2.addLayout(self.thumbnail_layout)
+        self.header_layout = QtGui.QHBoxLayout()
+        self.header_layout.setContentsMargins(-1, 0, -1, -1)
+        self.header_layout.setObjectName("header_layout")
+        self.header = QtGui.QLabel(self.box)
+        self.header.setObjectName("header")
+        self.header_layout.addWidget(self.header)
+        self.button = QtGui.QToolButton(self.box)
+        self.button.setObjectName("button")
+        self.header_layout.addWidget(self.button)
+        self.verticalLayout_2.addLayout(self.header_layout)
+        self.body = QtGui.QLabel(self.box)
+        self.body.setObjectName("body")
+        self.verticalLayout_2.addWidget(self.body)
+        self.verticalLayout.addWidget(self.box)
+
+        self.retranslateUi(ShotgunFolderWidget)
+        QtCore.QMetaObject.connectSlotsByName(ShotgunFolderWidget)
+
+    def retranslateUi(self, ShotgunFolderWidget):
+        ShotgunFolderWidget.setWindowTitle(QtGui.QApplication.translate("ShotgunFolderWidget", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.header.setText(QtGui.QApplication.translate("ShotgunFolderWidget", "header", None, QtGui.QApplication.UnicodeUTF8))
+        self.button.setText(QtGui.QApplication.translate("ShotgunFolderWidget", "Actions", None, QtGui.QApplication.UnicodeUTF8))
+        self.body.setText(QtGui.QApplication.translate("ShotgunFolderWidget", "body", None, QtGui.QApplication.UnicodeUTF8))
+
+from . import resources_rc
