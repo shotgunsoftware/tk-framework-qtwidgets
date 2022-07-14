c24acb52a759b4fe20656f65dcbe0696e32273e0
Stacey Oue <stacey.oue@autodesk.com>
2022-07-14 15:10:43 -0400

    Update build script.

--- python/shotgun_widget/resources/build_resources.sh
+++ python/shotgun_widget/resources/build_resources.sh
@@ -12,7 +12,8 @@
 
 # The path to output all built .py files to:
 UI_PYTHON_PATH=../ui
-PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python"
+# The path to where the PySide binaries are installed
+PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python/bin"
 
 # Remove any problematic profiles from pngs.
 for f in *.png; do mogrify $f; done
@@ -24,18 +25,17 @@ function build_qt {
     # compile ui to python
     $1 $2 > $UI_PYTHON_PATH/$3.py
 
-    # replace PySide imports with tank.platform.qt and remove line containing Created by date
-    sed -i "" -e "s/from PySide import/from tank.platform.qt import/g" -e "/# Created:/d" $UI_PYTHON_PATH/$3.py
+    # replace PySide imports with sgtk.platform.qt and remove line containing Created by date
+    sed -i "" -e "s/from PySide import/from sgtk.platform.qt import/g" -e "/# Created:/d" $UI_PYTHON_PATH/$3.py
 }
 
 function build_ui {
-    build_qt "pyside-uic --from-imports" "$1.ui" "$1"
+    build_qt "${PYTHON_BASE}/python ${PYTHON_BASE}/pyside-uic --from-imports" "$1.ui" "$1"
 }
 
 function build_res {
-    build_qt "pyside-rcc -py3" "$1.qrc" "$1_rc"
+    build_qt "${PYTHON_BASE}/pyside-rcc -py3" "$1.qrc" "$1_rc"
 }
-
 
 # build UI's:
 echo "building user interfaces..."
